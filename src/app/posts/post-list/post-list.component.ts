4d71247249cf4a4952fde8ff69dc40a5af766828
cpark307 <cpark307@gmail.com>
2023-08-30 22:18:03 -0500

    initial commit to mean-app

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+
+  posts: Post[] = [];
+  isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
+  private postsSub: Subscription;
+
+  constructor(
+    public postsService: PostsService
+  ){}
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.isLoading = false;
+        this.posts = posts;
+      });
+  }
+  
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
+  }
+
+  onChangedPage(pageChange: PageEvent) {
+    console.log(pageChange);
+  }
+
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+}
